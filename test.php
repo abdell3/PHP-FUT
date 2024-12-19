503e0c28da3932044024a9ed07da4ea83a8b892e
abdellah <abdo.abdell.2000@gmail.com>
2024-12-19 11:02:46 +0100

    creer le dashbord et ajouter quelques images

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+   $pseudo = "test";
+   $age = 78;
+   $email = "idcniejc@gmail.com";
+   $passwprd
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+    <p>name: <?= $pseudo ?></p>
+    <p>email: <?= $age ?></p>
+    <p>num: <?= $email ?> </p>
+</body>
+</html>
